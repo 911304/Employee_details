136d7a0b4316ff203cb641f7d178f91eba9f2600
911304 <veenarayanaikar@gmail.com>
2025-11-15 12:29:38 +0530

    Create file1.py

---
+++ file1.py
@@ -0,0 +1,8 @@
+emp_name="veena"
+emp_Id="280"
+salary="70000"
+address="Dharwad"
+print("employee nanme is:",emp_name)
+print("employee id is:",emp_Id)
+print("employee salary is:",salary)
+print("employee adsress is:",address)
